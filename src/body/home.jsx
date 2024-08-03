06e3064eff64833eee1743d77a02715bb4a35721
dilshodtm7 <155984333+dilshodtm7@users.noreply.github.com>
2024-08-04 01:27:16 +0500

    Update home.jsx

--- src/body/home.jsx
+++ src/body/home.jsx
@@ -13,7 +13,7 @@ const home = () => {
   useEffect(() => {
     if (WebApp.initDataUnsafe.user) {
       setUserData(WebApp.initDataUnsafe.user)
-      // localStorage.setItem("userInfoes", WebApp.WebAppInitData.start_param);
+      console.log("userInfoes", WebApp.initDataUnsafe);
       
     }
 
